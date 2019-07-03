27a35e6957acb866604c1449d805eb9a60d019f0
goranaviani <goranaviani@gmail.com>
2019-07-03 21:27:44 +0200

    updated api url to api/v1/ for preparation of api versioning

--- DjangoRestLeads/urls.py
+++ DjangoRestLeads/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('apiv1/', include('leads.urls')),
+    path('api/v1/', include('leads.urls')),
 ]
