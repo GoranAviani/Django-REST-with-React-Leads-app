dbb4756314e7e46cc5cad111a93df09a4093508d
goranaviani <goranaviani@gmail.com>
2019-07-10 21:06:27 +0200

    using APIView instead of ListAPIView

--- leads/urls.py
+++ leads/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('add-lead/', views.LeadListCreate.as_view() ),
-    path('all-leads/', views.LeadListShow.as_view() ),
+    path('all-leads/', views.ShowLeadsList.as_view() ),
 ]
--- leads/views.py
+++ leads/views.py
@@ -4,13 +4,22 @@ from django.shortcuts import render
 from leads.models import Lead
 from leads.serializers import LeadSerializer
 from rest_framework import generics
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+    
 
 class LeadListCreate(generics.ListCreateAPIView):
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
 
+#Depreciated due to using custom views class ShowLeadsList(APIView):
+'''
 class LeadListShow(generics.ListAPIView):
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
-    
+'''
+class ShowLeadsList(APIView):
+    def get(self, request):
+        leads = Lead.objects.all()[:20]
+        data = LeadSerializer(leads, many=True).data
+        return Response(data)
